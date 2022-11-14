26bfb6ba38e68acde32b70d51a938a6ec8dd5c7d
userRashid Gulamov <rashidgulamov7@gmail.com>
2022-11-15 01:05:01 +0600

    #1 Создано приложение accounts, в рамках которого создана модель Account и зарегистрирована в админке. Созданы миграции.
    Созданы формы регистрации и аутентификации. Добавлены шаблоны регистрации и аутентификации, а также профиля и изменения профиля, т.к. понадобятся в дальнейшей работе, при этом, еще не были реализованы.
    
    Созданы вьюшки регистрации, логина и логаута. Добавлены соответствующие пути.
    
    Использован базовый код из проекта instagram и переделан под требования текущего проекта.
    
    В настройках settings добавлено приложение accounts.

---
+++ headhunter/accounts/forms.py
@@ -0,0 +1,62 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.forms import TextInput
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True, label='Имя')
+    password = forms.CharField(required=True, label='Пароль', widget=forms.PasswordInput)
+    next = forms.CharField(required=False, widget=forms.HiddenInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password')
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Подтвердите пароль', strip=False, required=True,
+                                       widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'usertype', 'username', 'email', 'password', 'password_confirm', 'phone')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise ValidationError('Пароли не совпадают')
+        email = cleaned_data.get('email')
+        if not email:
+            raise ValidationError('Поле "Email" обязательно к заполнению')
+        phone = cleaned_data.get('phone')
+        if not phone:
+            raise ValidationError('Поле "Телефон" обязательно к заполнению')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+        if commit:
+            user.save()
+        return user
+
+
+# class UserChangeForm(forms.ModelForm):
+#     class Meta:
+#         model = get_user_model()
+#         fields = ('first_name', 'login', 'email', 'avatar')
+#         labels = {'first_name': 'Имя', 'login': 'Логин', 'email': 'Email'}
+#
+#
+# class SearchForm(forms.Form):
+#     search = forms.CharField(max_length=100, required=False, label='', widget=TextInput(attrs={
+#         'class': 'mr-3 ps-3 class-form border-0 border-top bg-light rounded',
+#         'style': 'width: 270px; height: 35px; outline:0px none transparent; overflow:auto; resize:none',
+#         'placeholder': 'Поиск',
+#     }
+#     ))
--- headhunter/accounts/views.py
+++ headhunter/accounts/views.py
@@ -1,3 +1,134 @@
-from django.shortcuts import render
+from urllib.parse import urlencode
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView, ListView
+
+from accounts.forms import LoginForm, CustomUserCreationForm
+from accounts.models import Account
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        next = request.GET.get('next')
+        form_data = {} if not next else {'next': next}
+        form = self.form(form_data)
+        context = {'login_form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('login')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        next = form.cleaned_data.get('next')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('login')
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+class RegisterView(CreateView):
+    model = get_user_model()
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+    success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.groups.add(1 if user.usertype == 'company' else 2)
+            login(request, user)
+            return redirect('index')
+        context = {}
+        context['form'] = form
+        return self.render_to_response(context)
+#
+#
+# class ProfileView(LoginRequiredMixin, DetailView):
+#     model = get_user_model()
+#     template_name = 'profile.html'
+#     context_object_name = 'user_obj'
+#
+#     def get_context_data(self, **kwargs):
+#         posts = self.object.posts.order_by('-created_at')
+#         own_profile = self.request.user
+#         some_profile = kwargs.get('object')
+#         kwargs['own_subscribes'] = own_profile.subscriptions.count()
+#         kwargs['own_subscribers'] = own_profile.subscribers.count()
+#         kwargs['some_subscribes'] = some_profile.subscriptions.count()
+#         kwargs['some_subscribers'] = some_profile.subscribers.count()
+#         kwargs['posts'] = posts
+#         kwargs['search_form'] = SearchForm()
+#         return super().get_context_data(**kwargs)
+#
+#     def get(self, request, *args, **kwargs):
+#         subscribe_to = request.GET.get('subscribe_to')
+#         if subscribe_to:
+#             request.user.subscriptions.add(subscribe_to)
+#         subscribe_of = request.GET.get('subscribe_of')
+#         if subscribe_of:
+#             request.user.subscriptions.remove(subscribe_of)
+#         return super().get(request, *args, **kwargs)
+#
+#
+# class UserChangeView(UpdateView):
+#     model = get_user_model()
+#     form_class = UserChangeForm
+#     template_name = 'user_change.html'
+#     context_object_name = 'user_obj'
+#
+#     def get_success_url(self):
+#         return reverse('profile', kwargs={'pk': self.object.pk})
+#
+#
+# class ProfilesView(ListView):
+#     template_name = 'profiles.html'
+#     model = get_user_model()
+#     context_object_name = 'profiles'
+#     queryset = Account.objects.all()
+#
+#     def get(self, request, *args, **kwargs):
+#         self.form = self.get_search_form()
+#         self.search_value = self.get_search_value()
+#         return super().get(request, *args, **kwargs)
+#
+#     def get_search_form(self):
+#         return SearchForm(self.request.GET)
+#
+#     def get_search_value(self):
+#         if self.form.is_valid():
+#             return self.form.cleaned_data.get('search')
+#         return None
+#
+#     def get_queryset(self):
+#         queryset = super().get_queryset().all()
+#         print(queryset)
+#         if self.search_value:
+#             query = Q(email__icontains=self.search_value) | Q(login__icontains=self.search_value) | Q(
+#                 first_name__icontains=self.search_value)
+#             queryset = queryset.filter(query)
+#         return queryset
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(object_list=object_list, **kwargs)
+#         context['form'] = self.form
+#         if self.search_value:
+#             context['query'] = urlencode({'search': self.search_value})
+#         return context
