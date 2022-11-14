ee889a2ec9e95d5444b4f46b8926a7fa1ce27658
userRashid Gulamov <rashidgulamov7@gmail.com>
2022-11-15 01:18:35 +0600

    #1 Дополнение к коммиту (модель Account)26bfb6ba38e68acde32b70d51a938a6ec8dd5c7d. Не попал в коммит по техническим причинам.

--- headhunter/accounts/models.py
+++ headhunter/accounts/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import TextChoices
 
-# Create your models here.
+
+class GenderChoices(TextChoices):
+    COMPANY = 'company', 'Компания'
+    USER = 'candidate', 'Кандидат'
+
+
+class Account(AbstractUser):
+    usertype = models.CharField(choices=GenderChoices.choices, verbose_name='Пользователь', null=False,
+                                blank=False, max_length=250)
+    username = models.CharField(verbose_name='Имя', unique=True, null=False, blank=False, max_length=150)
+    email = models.EmailField(verbose_name='Электронная почта', unique=True, null=False, blank=False)
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to='avatars',
+        verbose_name='Аватар'
+    )
+    phone = models.CharField(verbose_name='Телефона', null=False, blank=False, max_length=100)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
