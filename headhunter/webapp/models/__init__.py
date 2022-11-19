3673872c0b370b24dec78f8516d12fd441c755ca
Adlet Kurentaev <kurentaev@gmail.com>
2022-11-19 20:01:42 +0600

    Добавлена базовая модель, файл init.py. Также добавлены менеджер проекта и базовая разметка для проекта.

---
+++ headhunter/webapp/models/__init__.py
@@ -0,0 +1,6 @@
+from .base import BaseModel
+from .categories import CategoryChoices
+from .vacancies import Vacancies
+from .resumes import Resumes
+from .experiences import Experiences
+from .educations import Educations
