31c31df75e1ae2091473139c98be19fab8b04910
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-11-21 02:58:49 +0600

    #10 Созданы операций просмотра резюме, списка резюме, создания и изменения резюме

--- headhunter/accounts/urls.py
+++ headhunter/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from accounts.views import LoginView, logout_view, RegisterView, ProfileView, UserChangeView, PasswordChangeView
+from webapp.views.resumes import CreateResumeView, ListResumesView, ResumeView, EditResumeView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
@@ -8,5 +9,9 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('profile/<int:pk>/password-change/', PasswordChangeView.as_view(), name='password_change'),
-    path('profile/<int:pk>/change/', UserChangeView.as_view(), name='change')
+    path('profile/<int:pk>/change/', UserChangeView.as_view(), name='change'),
+    path('profile/<int:pk>/resumes/create', CreateResumeView.as_view(), name='create_resume'),
+    path('profile/<int:pk>/resumes/', ListResumesView.as_view(), name='resumes'),
+    path('profile/<int:upk>/resume/<int:pk>/', ResumeView.as_view(), name='resume'),
+    path('profile/<int:upk>/resume/<int:pk>/edit', EditResumeView.as_view(), name='edit_resume'),
 ]
--- headhunter/webapp/views/index.py
+++ headhunter/webapp/views/index.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from webapp.models import Resumes
 
 
 # Create your views here.
-class IndexView(TemplateView):
-    template_name = 'index.html'
+class IndexView(ListView):
+    template_name = 'resumes/resumes.html'
+    model = Resumes
+
+    def get_queryset(self):
+        queryset = Resumes.objects.filter(is_hidden=False)
+        return queryset
