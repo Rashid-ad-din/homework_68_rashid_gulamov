f9e989f37c996e949a234e36bc842c892887292c
Adlet Kurentaev <kurentaev@gmail.com>
2022-11-21 15:45:21 +0600

    #12 Добавлена CRUD для модели вакансии, прописаны пути, добавлены шаблоны

--- headhunter/accounts/urls.py
+++ headhunter/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from accounts.views import LoginView, logout_view, RegisterView, ProfileView, UserChangeView, PasswordChangeView
 from webapp.views.resumes import CreateResumeView, ListResumesView, ResumeView, EditResumeView
+from webapp.views.vacancies import CreateVacancyView, ListVacancyView, VacancyView, EditVacancyView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
@@ -14,4 +15,8 @@ urlpatterns = [
     path('profile/<int:pk>/resumes/', ListResumesView.as_view(), name='resumes'),
     path('profile/<int:upk>/resume/<int:pk>/', ResumeView.as_view(), name='resume'),
     path('profile/<int:upk>/resume/<int:pk>/edit', EditResumeView.as_view(), name='edit_resume'),
+    path('profile/<int:pk>/vacancies/create', CreateVacancyView.as_view(), name='create_vacancy'),
+    path('profile/<int:pk>/vacancies/', ListVacancyView.as_view(), name='vacancies'),
+    path('profile/<int:upk>/vacancies/<int:pk>/', VacancyView.as_view(), name='vacancy'),
+    path('profile/<int:upk>/vacancies/<int:pk>/edit', EditVacancyView.as_view(), name='edit_vacancy'),
 ]
