830b6e95698e8e93a61c86c74458fd00524f76d4
Adlet Kurentaev <kurentaev@gmail.com>
2022-11-24 22:35:45 +0600

    #10 Произведена раздача прав

--- headhunter/webapp/views/educations.py
+++ headhunter/webapp/views/educations.py
@@ -1,16 +1,16 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse
 from django.views.generic import CreateView, UpdateView, DeleteView
-
 from webapp.forms.educations import EducationForm
 from webapp.models import Resumes, Educations
 
 
-class CreateEducationView(LoginRequiredMixin, CreateView):
+class CreateEducationView(PermissionRequiredMixin, CreateView):
     template_name = 'educations/create_education.html'
     model = Educations
     form_class = EducationForm
+    permission_required = 'webapp.add_educations'
 
     def post(self, request, pk, *args, **kwargs):
         form = self.form_class(request.POST)
@@ -27,20 +27,32 @@ class CreateEducationView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse('profile', kwargs={'pk': self.request.user.pk})
 
+    def has_permission(self):
+        resume = get_object_or_404(Resumes, pk=self.kwargs.get('pk'))
+        return (super().has_permission() or resume.author.username == str(self.request.user)
+                or self.request.user.is_superuser)
 
-class EditEducationView(LoginRequiredMixin, UpdateView):
+
+class EditEducationView(PermissionRequiredMixin, UpdateView):
     template_name = 'educations/edit_education.html'
     model = Educations
     form_class = EducationForm
+    permission_required = 'webapp.change_educations'
 
     def get_success_url(self):
         return reverse('resume', kwargs={'pk': self.kwargs.get('upk')})
 
+    def has_permission(self):
+        resume = get_object_or_404(Resumes, pk=self.kwargs.get('pk'))
+        return (super().has_permission() or resume.author.username == str(self.request.user)
+                or self.request.user.is_superuser)
 
-class DeleteEducationView(LoginRequiredMixin, DeleteView):
+
+class DeleteEducationView(PermissionRequiredMixin, DeleteView):
     template_name = 'educations/delete_education.html'
     model = Educations
     context_object_name = 'education'
+    permission_required = 'webapp.delete_educations'
 
     def get(self, request, *args, **kwargs):
         self.resume_pk = kwargs.get('upk')
@@ -52,3 +64,8 @@ class DeleteEducationView(LoginRequiredMixin, DeleteView):
 
     def get_success_url(self):
         return reverse('resume', kwargs={'pk': self.kwargs.get('upk')})
+
+    def has_permission(self):
+        resume = get_object_or_404(Resumes, pk=self.kwargs.get('pk'))
+        return (super().has_permission() or resume.author.username == str(self.request.user)
+                or self.request.user.is_superuser)
--- headhunter/webapp/views/experiences.py
+++ headhunter/webapp/views/experiences.py
@@ -1,17 +1,16 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse
 from django.views.generic import CreateView, UpdateView, DeleteView
-
-from webapp.forms.educations import EducationForm
 from webapp.forms.experiences import ExperienceForm
 from webapp.models import Resumes, Educations, Experiences
 
 
-class CreateExperienceView(LoginRequiredMixin, CreateView):
+class CreateExperienceView(PermissionRequiredMixin, CreateView):
     template_name = 'experiences/create_experience.html'
     model = Experiences
     form_class = ExperienceForm
+    permission_required = 'webapp.add_experiences'
 
     def post(self, request, pk, *args, **kwargs):
         form = self.form_class(request.POST)
@@ -28,20 +27,32 @@ class CreateExperienceView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse('profile', kwargs={'pk': self.request.user.pk})
 
+    def has_permission(self):
+        resume = get_object_or_404(Resumes, pk=self.kwargs.get('pk'))
+        return (super().has_permission() or resume.author.username == str(self.request.user)
+                or self.request.user.is_superuser)
 
-class EditExperienceView(LoginRequiredMixin, UpdateView):
+
+class EditExperienceView(PermissionRequiredMixin, UpdateView):
     template_name = 'experiences/edit_experience.html'
     model = Experiences
     form_class = ExperienceForm
+    permission_required = 'webapp.change_experiences'
 
     def get_success_url(self):
         return reverse('resume', kwargs={'pk': self.kwargs.get('upk')})
 
+    def has_permission(self):
+        resume = get_object_or_404(Resumes, pk=self.kwargs.get('pk'))
+        return (super().has_permission() or resume.author.username == str(self.request.user)
+                or self.request.user.is_superuser)
 
-class DeleteExperienceView(LoginRequiredMixin, DeleteView):
+
+class DeleteExperienceView(PermissionRequiredMixin, DeleteView):
     template_name = 'educations/delete_education.html'
     model = Experiences
     context_object_name = 'experience'
+    permission_required = 'webapp.delete_experiences'
 
     def get(self, request, *args, **kwargs):
         self.resume_pk = kwargs.get('upk')
@@ -53,3 +64,8 @@ class DeleteExperienceView(LoginRequiredMixin, DeleteView):
 
     def get_success_url(self):
         return reverse('resume', kwargs={'pk': self.kwargs.get('upk')})
+
+    def has_permission(self):
+        resume = get_object_or_404(Resumes, pk=self.kwargs.get('pk'))
+        return (super().has_permission() or resume.author.username == str(self.request.user)
+                or self.request.user.is_superuser)
